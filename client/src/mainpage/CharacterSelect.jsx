da065726cb4e552e06a057ff3e6f99146867abba
Nex974 <115940008+Nex974@users.noreply.github.com>
2024-10-14 23:13:21 +0300

    Styling

--- client/src/mainpage/CharacterSelect.jsx
+++ client/src/mainpage/CharacterSelect.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
 
 function CharacterSelect() {
-    return <h1>CharacterSelect</h1>;
-    // TODO: basic character looks 4-5, scroller to switch between them
+    return (
+        <div id="default-carousel" className="relative w-full" data-carousel="slide">
+            <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+                <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="/docs/images/carousel/carousel-1.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                </div>
+                <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="/docs/images/carousel/carousel-2.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                </div>
+                <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="/docs/images/carousel/carousel-3.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                </div>
+                <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="/docs/images/carousel/carousel-4.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                </div>
+                <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                     <img src="/docs/images/carousel/carousel-5.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                </div>
+            </div>
+            <button type="button" className="!bg-transparent absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                <span className="inline-flex items-center justify-center w-10 h-10 rounded-full !bg-white !dark:bg-gray-800/30 !group-hover:bg-white/50 !dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg className="w-4 h-4 !text-purple-600 dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
+                    </svg>
+                    <span className="sr-only">Previous</span>
+                </span>
+            </button>
+            <button type="button" className="!bg-transparent absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                <span className="inline-flex items-center justify-center w-10 h-10 rounded-full !bg-white !dark:bg-gray-800/30 !group-hover:bg-white/50 !dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg className="w-4 h-4 !text-purple-600 dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
+                    </svg>
+                    <span className="sr-only">Next</span>
+                </span>
+            </button>
+        </div>
+    )
 }
 
 export default CharacterSelect;
--- client/src/mainpage/StartMenu.jsx
+++ client/src/mainpage/StartMenu.jsx
@@ -1,34 +1,48 @@
-import React from 'react';
-//import { useDispatch, useSelector } from 'react-redux';
-//import { useNavigate } from 'react-router-dom'
-import carImage from '../styles/car.jpg'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import CharacterSelect from './CharacterSelect';
 
 function StartMenu() {
+  const [playerName, setPlayerName] = useState("");
   //const dispatch = useDispatch();
   //const navigate = useNavigate();
+
+  function handleInputChange(event) {
+    setPlayerName(event.target.value)
+  }
     
 
   return (
-<div className="grid place-items-center min-h-screen">
+    
+<div className="grid place-items-center min-h-screen" style={{ marginTop: '-40px' }}>
 <div className="flex font-sans">
-<div className="flex-none w-56 relative">
-  <img src={carImage} alt="" className="absolute inset-0 w-full h-full object-cover rounded-lg" loading="lazy" />
+<div className="flex-none w-80" style={{ marginTop: '-70px' }}>
+  <CharacterSelect />
 </div>
-  <form className="flex-auto p-6">
+  <form className="flex-auto px-9">
     <div className="flex flex-wrap">
-      <div className="w-full flex-none mt-2 order-1 text-5xl font-bold text-violet-600">
+      <div className="w-full flex-none mt-2 mb-6 order-1 text-5xl font-bold text-violet-600">
         edno
       </div>
     </div>
-    <div className="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
-      <div className="space-x-2 flex text-sm font-bold"></div>
-    </div>
+    <div className="mt-5 mb-1 pb-6">
+        <input
+          type="text"
+          id="playerName"
+          name="playerName"
+          className="mt-1 block w-full px-3 py-2 text-violet-600 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-violet-500 focus:border-violet-500 sm:text-sm"
+          placeholder="Your username"
+          value={playerName}
+          onChange={handleInputChange}
+        />
+      </div>
     <div className="flex space-x-4 mb-5 text-sm font-medium">
       <div className="flex-auto flex space-x-4">
-        <button className="h-10 px-6 font-semibold rounded-full bg-violet-600 text-white" type="submit">
+        <button className="flex items-center h-10 px-6 font-semibold rounded-full bg-violet-600 text-white" type="submit">
           Join Game
         </button>
-        <button className="h-10 px-6 font-semibold rounded-full border border-slate-200 text-slate-900" type="button">
+        <button className="bg-sky-400 flex items-center h-10 px-6 font-semibold rounded-full border border-slate-200 text-slate-900" type="button">
           Create Game
         </button>
       </div>
