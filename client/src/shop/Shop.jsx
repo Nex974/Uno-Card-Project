5b07e96951a3c05491fecb260c8bc147f64783ba
Nex974 <115940008+Nex974@users.noreply.github.com>
2024-10-23 23:57:19 +0300

    basic template for shop

--- client/src/shop/Shop.jsx
+++ client/src/shop/Shop.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { toggleShop } from '../store';
 
 function Shop() {
     const dispatch = useDispatch();
-    const openShop = useSelector((state) => state.game.openShop);
 
     function handleShopToggle() {
         dispatch(toggleShop())
