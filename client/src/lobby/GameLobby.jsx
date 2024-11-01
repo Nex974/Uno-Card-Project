ce9fd4fbb70df18b72b76b6c4bfe00658b094cb8
Nex974 <115940008+Nex974@users.noreply.github.com>
2024-11-01 16:39:17 +0200

    separated fetchLobbies into an utils folder

--- client/src/lobby/GameLobby.jsx
+++ client/src/lobby/GameLobby.jsx
@@ -1,22 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import WrongLobby from './WrongLobby.jsx'
+import { setOpenLobbies } from '../store';
+import { useWebSocket } from '../WebSocketProvider';
+import fetchLobbies from '../utils/UpdateLobbies';
+import WrongLobby from './WrongLobby.jsx';
 
 function GameLobby() {
-  const { gameId } = useParams();
-  const navigate = useNavigate();
-  const lobbyExists = useSelector((state) => state.menu.lobbyExists);
+    const { gameId } = useParams();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const socket = useWebSocket();
+    const isConnected = useSelector((state) => state.menu.webSocket);
+    const openLobbies = useSelector((state) => state.menu.openLobbies);
+    const [lobbyExists, setLobbyExists] = useState(false);
 
-  if (!lobbyExists) { 
-    return <WrongLobby />;
-  }
-  return (
-    <div>
-      <h1 className="text-3xl font-bold text-center">Game ID: {gameId}</h1>
-      {/* Game logic and UI go here */}
-    </div>
-  );
+    useEffect(() => {
+        if (socket) {
+            fetchLobbies(socket, dispatch, isConnected, setOpenLobbies);
+        }
+    }, [socket, dispatch]);
+
+    useEffect(() => {
+        const exists = Object.values(openLobbies).some((lobby) => lobby.gameId === gameId);
+        setLobbyExists(exists);
+    }, [gameId, openLobbies]);
+
+    if (!lobbyExists) { 
+        return <WrongLobby />;
+    }
+
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-center">Game ID: {gameId}</h1>
+            {/* Game logic and UI go here */}
+        </div>
+    );
 }
 
 export default GameLobby;
