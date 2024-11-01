852bcec7d5b1b2544fa24e30e0577033961e5c53
Nex974 <115940008+Nex974@users.noreply.github.com>
2024-11-01 16:41:42 +0200

    added fetchLobbies to fix a bug

--- client/src/lobby/JoinLobby.jsx
+++ client/src/lobby/JoinLobby.jsx
@@ -2,52 +2,48 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setOpenLobbies } from '../store';
 import { useWebSocket } from '../WebSocketProvider';
+import { useNavigate } from 'react-router-dom';
+import fetchLobbies from '../utils/UpdateLobbies';
 
 function JoinLobby() {
-  const dispatch = useDispatch();
-  const socket = useWebSocket();
-  const isConnected = useSelector((state) => state.menu.webSocket);
-  const openLobbies = useSelector((state) => state.menu.openLobbies);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const socket = useWebSocket();
+    const isConnected = useSelector((state) => state.menu.webSocket);
+    const openLobbies = useSelector((state) => state.menu.openLobbies);
 
-  useEffect(() => {
-    if (socket) {
-      socket.onmessage = (event) => {
-        const message = JSON.parse(event.data);
-        if (message.type === 'UPDATE_LOBBIES') {
-          dispatch(setOpenLobbies(message.payload));
-          console.log('Lobbies updated:', message.payload);
+    useEffect(() => {
+        if (socket) {
+            fetchLobbies(socket, dispatch, isConnected, setOpenLobbies);
+
+            // Cleanup function to avoid memory leaks
+            return () => {
+                socket.onmessage = null; // Clean up the message handler
+            };
         }
-      };
+    }, [socket, isConnected, dispatch]);
 
-      if (isConnected) {
-        const fetchLobbiesMessage = JSON.stringify({ type: 'FETCH_LOBBIES' });
-        socket.send(fetchLobbiesMessage);
-      }
-    }
-
-    return () => {
-      if (socket) {
-        socket.onmessage = null;
-      }
-    };
-  }, [socket, isConnected, dispatch]);
-
-  return (
-    <div>
-      <h1>Join Lobby</h1>
-      <ul>
-        {openLobbies.length > 0 ? (
-          openLobbies.map((lobby) => (
-            <li key={lobby.gameId}>
-              {lobby.lobbyName} - {lobby.maxPlayers} players
-            </li>
-          ))
-        ) : (
-          <p>No open lobbies available.</p>
-        )}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="text-center">Join Lobby</h1>
+            <div>
+                <ul>
+                    {openLobbies.length > 0 ? (
+                        openLobbies.map((lobby) => (
+                            <li key={lobby.gameId} className='text-center'>
+                                {lobby.lobbyName} - {lobby.players.length}/{lobby.maxPlayers} players
+                                <button onClick={() => navigate(`/game-lobby/${lobby.gameId}`)}>
+                                    Join Lobby
+                                </button>
+                            </li>
+                        ))
+                    ) : (
+                        <p className='text-center'>No open lobbies available.</p>
+                    )}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default JoinLobby;
